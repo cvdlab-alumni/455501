6d67ae19a484efe50fba0ed5644f8f68e590dd36
Oscare <oscareijsermans@hotmail.com>
2014-03-22 08:16:45 +0100

    struttura cartelle homework1

--- learning-javascript/2013-03-08/exercise03a.js
+++
@@ -1,9 +0,0 @@
-function capitalizeString(inputString) 
-{
-	return inputString[0].toUpperCase() + inputString.slice(1);
-}
-
-var outputString = capitalizeString("ciao");
-
-console.log(outputString);
-
--- learning-javascript/2013-03-21/exercise01.js
+++
@@ -1,9 +0,0 @@
-function Point2D(x, y)
-{
-	this.x = x;
-	this.y = y;
-}
-
-var point2d = new Point2D(2, 2);
-
-point2d;
